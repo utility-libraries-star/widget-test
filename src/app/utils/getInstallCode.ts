1aa2f70fb9428dfbdc4d61b4b53bda11df786b12
a.movchan <a.movchan@elfsight.com>
2024-10-10 17:55:19 +0300

    Upload

--- src/app/utils/getInstallCode.ts
+++ src/app/utils/getInstallCode.ts
@@ -5,6 +5,6 @@ export function getInstallCode(settings: WidgetProps) {
   const widgetId = uniqID();
   return `<div id='widget-${widgetId}'></div>
 <script>
-\tfunction installStyles(){let e=window.location.origin+"/public/style.css",t=document.querySelector('link[href="'+e+'"]');if(!t){let l=document.createElement("link");l.rel="stylesheet",l.href=e,document.head.appendChild(l)}}function installScript(e){let t=window.location.origin+"/public/bundle.js",l=document.querySelector('script[src="'+t+'"]');if(l)e();else{let i=document.createElement("script");i.src=t,i.addEventListener("load",e),document.head.appendChild(i)}}document.addEventListener("DOMContentLoaded",()=>{let e=document.querySelector('#widget-'+"${widgetId}");e&&(installStyles(),installScript(()=>{Widget.createWidget(e,${JSON.stringify(settings)})}))});
+\tfunction installStyles(){let e=${window.location.origin}+"/public/style.css",t=document.querySelector('link[href="'+e+'"]');if(!t){let l=document.createElement("link");l.rel="stylesheet",l.href=e,document.head.appendChild(l)}}function installScript(e){let t=${window.location.origin}+"/public/bundle.js",l=document.querySelector('script[src="'+t+'"]');if(l)e();else{let i=document.createElement("script");i.src=t,i.addEventListener("load",e),document.head.appendChild(i)}}document.addEventListener("DOMContentLoaded",()=>{let e=document.querySelector('#widget-'+"${widgetId}");e&&(installStyles(),installScript(()=>{Widget.createWidget(e,${JSON.stringify(settings)})}))});
 </script>`;
 }
